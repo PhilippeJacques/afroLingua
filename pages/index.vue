fe831a24fa38a1a33e513a760cae564af7916218
PhilippeJacques <57878727+PhilippeJacques@users.noreply.github.com>
2025-12-25 14:03:11 +0100

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+    data() {
+        return {
+            showMail: false,}
+    },
+
+    setup() {
+        const title = "Afrolingo | Apprendre les langues africaines"
+        useHead({
+            title,
+            meta: [
+                {
+                    name: 'description',
+                    content: `Apprenez à parler une langue africaine en peu de temps. Avec Afrolingo, l'apprentissage de ces langues n'a jamais été aussi accessible et facile !`
+                },
+                {
+                    hid: 'og-desc',
+                    property: 'og:description',
+                    content: `Apprenez à parler une langue africaine en peu de temps. Avec Afrolingo, l'apprentissage de ces langues n'a jamais été aussi accessible et facile !`
+                },
+                {
+                    hid: 'og-type',
+                    property: 'og:type',
+                    content: `website`
+                },
+                {
+                    hid: 'og-image',
+                    property: 'og:image',
+                    content: `/cover.jpg`
+                },
+                {
+                    hid: 't-image',
+                    property: 'twitter:image',
+                    content: `/cover.jpg`
+                },
+            ]
+        })
+    },
+}
+</script>
+<template>
+    <OrganismMenu />
+    <OrganismHeader @openContact="showMail = true"/>
+    <SectionsFeatures />
+    <SectionsVision @openContact="showMail = true" class="mt-12 sm:mt-28" />
+    <SectionsImg />
+    <SectionsFamily @openContact="showMail = true" />
+    <SectionsPartner />
+    <SectionsContact />
+    <SectionsFooter @openContact="showMail = true" />
+    <OrganismGetEmail @close="showMail = false" v-if="showMail" />
+</template>
+<style scoped></style>
